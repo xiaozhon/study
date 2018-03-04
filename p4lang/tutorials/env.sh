64cc217deca1285bdc4f397516fe2a1781438c5e
xiaozhon <xiaoyang399@gmail.com>
2018-03-03 22:45:04 -0500

    finished hello_world for p4

---
+++ p4lang/tutorials/env.sh
@@ -0,0 +1,7 @@
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# ---------------- EDIT THIS ------------------
+BMV2_PATH=/opt/github/p4lang/bmv2
+P4C_BM_PATH=/opt/github/p4lang/p4c-bmv2
+# ---------------- END ------------------
+ls 
